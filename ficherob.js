6a42ed836b6ecb2044e83621981418efc69484f4
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-12 18:32:50 +0200

    actualización segundo fichero

--- ficherob.js
+++ ficherob.js
@@ -1,3 +1,3 @@
-console.log("----------------")
-console.log("Soy el fichero B")
-console.log("----------------")
+console.log("****************")
+console.log("Fichero II")
+console.log("****************")
